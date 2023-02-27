2b45afef4d460e6cf84032e305f3b78b0fb599ca
Cédric Jonkers <Jonkerscedric@gmail.com>
2023-02-27 23:56:27 +0100

    fix

--- js/main.js
+++ js/main.js
@@ -1,23 +1,69 @@
-const users = document.querySelectorAll('.user');
+const userOneCanvas = document.getElementById('user-one-canvas');
+const userOneButton = document.getElementById('user-one-button');
+const userTwoCanvas = document.getElementById('user-two-canvas');
+const userTwoButton = document.getElementById('user-two-button');
+const resultDiv = document.getElementById('result');
 
-function pickRandomFinger() {
-  const fingers = ['thumb', 'index finger', 'middle finger', 'ring finger', 'pinky'];
-  const randomIndex = Math.floor(Math.random() * fingers.length);
-  return fingers[randomIndex];
+let userOneFinger = null;
+let userTwoFinger = null;
+
+function drawCanvas(canvas, color) {
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.arc(canvas.width / 2, canvas.height / 2, canvas.width / 4, 0, 2 * Math.PI);
+  ctx.fill();
 }
 
-users.forEach(user => {
-  const button = user.querySelector('button');
-  const result = user.querySelector('.result');
-  button.addEventListener('touchstart', function(event) {
+function clearCanvas(canvas) {
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function detectTouch(canvas, finger) {
+  canvas.addEventListener('touchstart', function(event) {
     event.preventDefault();
-    const finger = pickRandomFinger();
-    result.innerHTML = `You picked ${finger}`;
+    const touch = event.touches[0];
+    const rect = canvas.getBoundingClientRect();
+    const x = touch.clientX - rect.left;
+    const y = touch.clientY - rect.top;
+    const radius = canvas.width / 4;
+    const distance = Math.sqrt(Math.pow(x - canvas.width / 2, 2) + Math.pow(y - canvas.height / 2, 2));
+    if (distance < radius) {
+      drawCanvas(canvas, 'green');
+      finger = true;
+      checkReady();
+    }
   });
-  button.addEventListener('touchmove', function(event) {
-    event.preventDefault();
-  });
-  button.addEventListener('touchend', function(event) {
-    event.preventDefault();
-  });
+}
+
+function checkReady() {
+  if (userOneFinger && userTwoFinger) {
+    userOneButton.disabled = true;
+    userTwoButton.disabled = true;
+    const result = Math.floor(Math.random() * 2) + 1;
+    if (result === 1) {
+      resultDiv.innerText = 'User 1 wins!';
+    } else {
+      resultDiv.innerText = 'User 2 wins!';
+    }
+    resultDiv.style.display = 'block';
+  }
+}
+
+detectTouch(userOneCanvas, userOneFinger);
+detectTouch(userTwoCanvas, userTwoFinger);
+
+userOneButton.addEventListener('click', function() {
+  userOneFinger = true;
+  drawCanvas(userOneCanvas, 'green');
+  userOneButton.disabled = true;
+  checkReady();
+});
+
+userTwoButton.addEventListener('click', function() {
+  userTwoFinger = true;
+  drawCanvas(userTwoCanvas, 'green');
+  userTwoButton.disabled = true;
+  checkReady();
 });
