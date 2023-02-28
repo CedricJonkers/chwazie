846938227ad5c5ec35336425daf9dd16f334145d
Cédric Jonkers <Jonkerscedric@gmail.com>
2023-02-28 01:10:57 +0100

    mobile

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // Get the circle element
 const circle = document.getElementById('circle');
 
-// Add a mousedown event listener to the document
-document.addEventListener('mousedown', function(event) {
+// Add a touchstart event listener to the document
+document.addEventListener('touchstart', function (event) {
+
   // Show the circle
   circle.style.display = 'block';
 
@@ -12,28 +13,34 @@ document.addEventListener('mousedown', function(event) {
   // Set the background color of the circle element to the random color
   circle.style.backgroundColor = randomColor;
 
-  // Move the circle to the cursor position
-  circle.style.left = event.clientX - (circle.offsetWidth / 2) + 'px';
-  circle.style.top = event.clientY - (circle.offsetHeight / 2) + 'px';
+  // Move the circle to the touch position
+  circle.style.left = event.touches[0].clientX - (circle.offsetWidth / 2) + 'px';
+  circle.style.top = event.touches[0].clientY - (circle.offsetHeight / 2) + 'px';
 
-  // Add a mousemove event listener to the document
-  document.addEventListener('mousemove', moveCircle);
-});
+  // Add a touchmove event listener to the document
+  document.addEventListener('touchmove', moveCircle, { passive: false });
 
-// Add a mouseup event listener to the document
-document.addEventListener('mouseup', function() {
-  // Hide the circle
-  circle.style.display = 'none';
+  // Add a touchend event listener to the document
+  document.addEventListener('touchend', function () {
+    // Hide the circle
+    circle.style.display = 'none';
 
-  // Remove the mousemove event listener from the document
-  document.removeEventListener('mousemove', moveCircle);
+    // Remove the touchmove event listener from the document
+    document.removeEventListener('touchmove', moveCircle);
+
+    // Remove the touchend event listener from the document
+    document.removeEventListener('touchend', arguments.callee);
+  }, { once: true });
 });
 
 // Define the moveCircle function
 function moveCircle(event) {
-  // Move the circle to the cursor position
-  circle.style.left = event.clientX - (circle.offsetWidth / 2) + 'px';
-  circle.style.top = event.clientY - (circle.offsetHeight / 2) + 'px';
+  // Prevent the default touch behavior
+  event.preventDefault();
+
+  // Move the circle to the touch position
+  circle.style.left = event.touches[0].clientX - (circle.offsetWidth / 2) + 'px';
+  circle.style.top = event.touches[0].clientY - (circle.offsetHeight / 2) + 'px';
 }
 
 // Define the getRandomColor function
