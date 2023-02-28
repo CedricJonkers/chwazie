da100f4e316ddc07df16bbfc0a6519adebdd8cd8
Cédric Jonkers <Jonkerscedric@gmail.com>
2023-02-28 01:00:37 +0100

    drawcircle around finger

--- js/app.js
+++ js/app.js
@@ -1,34 +1,51 @@
-const screen1 = document.getElementById('screen1');
-const screen2 = document.getElementById('screen2');
-const startButton = document.getElementById('startButton');
+// Get the circle element
+const circle = document.getElementById('circle');
 
-let touchPoints = [];
+// Add a mousedown event listener to the document
+document.addEventListener('mousedown', function(event) {
+  // Show the circle
+  circle.style.display = 'block';
 
-screen1.addEventListener('touchstart', handleTouch);
-screen2.addEventListener('touchstart', handleTouch);
-screen1.addEventListener('click', handleTouch);
-screen2.addEventListener('click', handleTouch);
+  // Generate a random color
+  const randomColor = getRandomColor();
 
-function handleTouch(event) {
-  const touchX = event.pageX - event.target.offsetLeft;
-  const touchY = event.pageY - event.target.offsetTop;
-  const circle = document.createElement('div');
-  circle.classList.add('circle');
-  circle.style.left = touchX + 'px';
-  circle.style.top = touchY + 'px';
-  event.target.appendChild(circle);
-  touchPoints.push({x: touchX, y: touchY});
+  // Set the background color of the circle element to the random color
+  circle.style.backgroundColor = randomColor;
+
+  // Move the circle to the cursor position
+  circle.style.left = event.clientX - (circle.offsetWidth / 2) + 'px';
+  circle.style.top = event.clientY - (circle.offsetHeight / 2) + 'px';
+
+  // Add a mousemove event listener to the document
+  document.addEventListener('mousemove', moveCircle);
+});
+
+// Add a mouseup event listener to the document
+document.addEventListener('mouseup', function() {
+  // Hide the circle
+  circle.style.display = 'none';
+
+  // Remove the mousemove event listener from the document
+  document.removeEventListener('mousemove', moveCircle);
+});
+
+// Define the moveCircle function
+function moveCircle(event) {
+  // Move the circle to the cursor position
+  circle.style.left = event.clientX - (circle.offsetWidth / 2) + 'px';
+  circle.style.top = event.clientY - (circle.offsetHeight / 2) + 'px';
 }
 
-startButton.addEventListener('click', startGame);
+// Define the getRandomColor function
+function getRandomColor() {
+  // Generate a random integer between 0 and 255 for each RGB component
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
 
-function startGame() {
-  const winningIndex = Math.floor(Math.random() * touchPoints.length);
-  const winningTouch = touchPoints[winningIndex];
-  const winningCircle = document.createElement('div');
-  winningCircle.classList.add('circle', 'highlight');
-  winningCircle.style.left = winningTouch.x + 'px';
-  winningCircle.style.top = winningTouch.y + 'px';
-  screen1.appendChild(winningCircle);
-  screen2.appendChild(winningCircle.cloneNode(true));
+  // Construct the color string in RGB format
+  const color = `rgb(${red}, ${green}, ${blue})`;
+
+  // Return the color string
+  return color;
 }
